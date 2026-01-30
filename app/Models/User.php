472cc97b648afe46d6d5cb873600ac533c214ca1
Pranay Baddam <pranayb2773@gmail.com>
2026-01-30 16:45:45 +0000

    Add Prettier scripts

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
