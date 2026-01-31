d610b3f9890274f1a56ac94e766bceef104ad30a
Pranay Baddam <pranayb2773@gmail.com>
2026-01-31 11:33:09 +0000

    Add default English language files for validation, authentication, pagination, and passwords

---
+++ lang/en/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'inactive' => 'Your account is inactive.',
+    'not_admin' => 'You do not have admin access.',
+    'password' => 'The provided password is incorrect.',
+    'verification_sent' => 'A new verification link has been sent to the email address you provided during registration.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
