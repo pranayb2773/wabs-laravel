049307c5836ae3617573645cf67920b32efebeca
Pranay Baddam <pranayb2773@gmail.com>
2026-01-31 11:27:51 +0000

    Introduce `auth` layout and `head` partial templates for authentication pages

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,13 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<title>{{ $title ?? config('app.name') }}</title>
+
+<link rel="icon" href="{{ asset('images/logo.webp') }}" type="image/png" sizes="18x18" />
+<link rel="apple-touch-icon" href="{{ asset('images/logo.webp') }}" />
+
+<link rel="preconnect" href="https://fonts.bunny.net" />
+<link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+@fluxAppearance
